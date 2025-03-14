1fb6f5012d286792b620c4207523d79a2b8fc6a6
Paradise-Is <1955692608@qq.com>
2025-03-14 15:55:39 +0800

    使用SQLite作为数据库，初步实现了登录注册等相关功能，完善了登陆注册相关逻辑

---
+++ app/src/main/java/com/example/smarttourism/activity/AdminLoginActivity.java
@@ -0,0 +1,119 @@
+package com.example.smarttourism.activity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.smarttourism.R;
+import com.example.smarttourism.util.DBHelper;
+
+public class AdminLoginActivity extends Activity {
+    private EditText usernameText;
+    private EditText passwordText;
+    private TextView backBt;
+    private Button adminLoginBt;
+    private ImageView ifPwdShow;
+    //输入框密码是否是隐藏，默认为true
+    private boolean isHide = false;
+    private DBHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //构建管理员登录界面
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_login);
+        //获取组件
+        usernameText=(EditText)this.findViewById(R.id.username);
+        passwordText=(EditText)this.findViewById(R.id.password);
+        backBt=(TextView)this.findViewById(R.id.backBt);
+        adminLoginBt=(Button)this.findViewById(R.id.adminLoginBt);
+        //实现数据库功能
+        dbHelper = DBHelper.getInstance(this);
+        dbHelper.open();
+        //实现密码显示与隐藏
+        ifPwdShow=findViewById(R.id.hide_or_display);
+        ifPwdShow.setImageResource(R.mipmap.password_hide);
+        //动态修改TransformationMethod实现密码是否可见
+        ifPwdShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isHide) {
+                    ifPwdShow.setImageResource(R.mipmap.password_display);
+                    isHide = true;
+                    //密码可见
+                    passwordText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }else{
+                    ifPwdShow.setImageResource(R.mipmap.password_hide);
+                    isHide = false;
+                    //密码不可见
+                    passwordText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+        //返回按钮响应
+        backBt.setOnClickListener(new BackBtLister());
+        //管理员登录按钮响应
+        adminLoginBt.setOnClickListener(new AdminLoginBtLister());
+    }
+
+    private class BackBtLister implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            //实现监听按钮，点击跳转
+            Intent intent = new Intent();
+            //前面为目前页面，后面为要跳转的下一个页面
+            intent.setClass(AdminLoginActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private class AdminLoginBtLister implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            String username=usernameText.getText().toString();
+            String password=passwordText.getText().toString();
+            //实现登录逻辑
+            if(username.equals("")){
+                Toast.makeText(AdminLoginActivity.this, "用户名输入不能为空", Toast.LENGTH_SHORT).show();
+            }
+            else if(password.equals("")){
+                Toast.makeText(AdminLoginActivity.this, "密码输入不能为空", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                String selection = "username=?";
+                String[] selectionArgs = {username};
+                //查询对应项
+                Cursor cursor = dbHelper.getDatabase().query("Admin", null, selection, selectionArgs, null, null, null);
+                //游标移动进行校验
+                if (cursor.moveToNext()) {
+                    //从数据库获取密码进行校验
+                    @SuppressLint("Range")
+                    String dbPassword = cursor.getString(cursor.getColumnIndex("password"));
+                    //关闭游标
+                    cursor.close();
+                    if (password.equals(dbPassword)) {
+                        Toast.makeText(AdminLoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                        //携带用户名数据跳转
+                        Intent intent = new Intent();
+                        intent.putExtra("username", username);
+                        intent.setClass(AdminLoginActivity.this, AdminMainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(AdminLoginActivity.this, "用户名或密码不正确", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(AdminLoginActivity.this, "用户名或密码不正确", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/smarttourism/util/DBHelper.java
@@ -0,0 +1,76 @@
+package com.example.smarttourism.util;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    //数据库名
+    private static final String DATABASE_NAME = "SmartTourism.db";
+    //数据库版本号
+    private static final int DATABASE_VERSION = 1;
+    //数据库操作实例
+    private static DBHelper instance;
+    private SQLiteDatabase database;
+
+    //创建用户信息表
+    private static final String CREATE_User = "create table if not exists User("
+            +"username text primary key,"
+            +"password text,"
+            +"email text,"
+            +"phone text,"
+            +"name text,"
+            +"birthday text,"
+            +"gender text)";
+
+    //创建管理员信息表
+    private static final String CREATE_Admin = "create table if not exists Admin("
+            +"username text primary key,"
+            +"password text)";
+
+    //构造函数，创建数据库
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    //获取数据库操作实例
+    public static synchronized DBHelper getInstance(Context context) {
+        if (instance == null) {
+            instance = new DBHelper(context.getApplicationContext());
+        }
+        return instance;
+    }
+    public SQLiteDatabase getDatabase(){
+        return database;
+    }
+
+    //创建数据库表
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_User);
+        db.execSQL(CREATE_Admin);
+        //初始化创建一个管理员账号
+        db.execSQL("insert into Admin(username,password)values('admin','123456')");
+    }
+
+    //数据库版本更新
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists User");
+        db.execSQL("drop table if exists Admin");
+        onCreate(db);
+    }
+
+    //实现数据库连接
+    public void open() {
+        //获取可写数据库
+        database = this.getWritableDatabase();
+    }
+
+    //数据库连接断开
+    public void close() {
+        if (database != null) {
+            database.close();
+        }
+    }
+}
